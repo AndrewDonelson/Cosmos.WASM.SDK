e888b9ad8fc5b52380123cde978f4188cef071de
adonelson <adonelson@affiniongroup.com>
2023-01-07 10:56:22 -0600

    add main loop with channel block added imports started on JS funcs & exports

--- internal/common/const.go
+++ internal/common/const.go
@@ -1,5 +1,7 @@
 package common
 
-const YAML_DIR = "/home/andrew/go/src/github.com/NlaakStudiosLLC/io.gwf.chain/docs/static/"
-const YAML_FILE = "openapi.yml"
-const DEFAULT_LINE_LENGTH = 100
+const (
+	YAML_DIR            = "/home/andrew/go/src/github.com/NlaakStudiosLLC/io.gwf.chain/docs/static/"
+	YAML_FILE           = "openapi.yml"
+	DEFAULT_LINE_LENGTH = 100
+)
